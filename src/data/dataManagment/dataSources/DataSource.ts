974c06b59bec5bb37daa45ca3d0ed6b629f72028
Adam Pelc <sikulin94@gmail.com>
2025-03-18 09:59:32 +0200

    .

--- src/data/dataManagment/dataSources/DataSource.ts
+++ src/data/dataManagment/dataSources/DataSource.ts
@@ -1,12 +1,34 @@
 import { DataSourceState, IDataSource, LoadData } from "data/dataManagment/DataManagmentTypes";
 
+// type CreateDataSourceReturn<Data, State extends DataSourceState<Data>> = IDataSource<Data, State, Load>;
+
+const createDataSource = <Data, State extends DataSourceState<Data>, Load extends LoadData<State>>({
+  initState,
+}: {
+  initState: () => State;
+}): IDataSource<Data, State, Load> => {
+  let state: State = initState();
+
+  const getState = () => ({ ...state });
+  const getData = () => state.data;
+  const reset = () => {
+    state = initState();
+  };
+
+  return {
+    getState,
+    getData,
+    reset,
+  };
+};
+
 class DataSource<Data, State extends DataSourceState<Data>, Load extends LoadData<Data, State>>
   implements IDataSource<Data, State, Load>
 {
   constructor({ initState, loadData }: { initState: () => State; loadData: Load }) {
     this.state = initState();
     this.initState = initState;
-    this.loadData = loadData
+    this.loadData = loadData;
   }
 
   private state: State;
@@ -21,8 +43,8 @@ class DataSource<Data, State extends DataSourceState<Data>, Load extends LoadDat
   };
 
   public load: Load = async (...args) => {
-    this.state = await this.loadData(...args)
-    
+    this.state = await this.loadData(...args);
+
     return this.state;
   };
 }
